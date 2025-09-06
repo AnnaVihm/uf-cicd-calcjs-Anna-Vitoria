31d02bad834721dccf6047a3c1b0af34327a3d2d
Anna Vitória <annavitoria12.mendes05@gmail.com>
2025-09-06 17:39:13 -0300

    adicionado função de multiplicação

--- src/calculadora.js
+++ src/calculadora.js
@@ -18,3 +18,15 @@ module.exports = {
     add,
     subtract
 };
+
+// Função de multiplicação
+function multiply(a, b) {
+    return a * b;
+}
+
+// Exportar as funções
+module.exports = {
+    add,
+    subtract,
+    multiply
+};
